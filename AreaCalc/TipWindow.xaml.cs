b2462c136fc5ae7a70f33c57bd5001781f4a6072
Степан Юзефович <yuzefovich1stepan@gmail.com>
2024-10-15 13:08:58 +0300

    основная концепция кода заработал добавил подсказку правильного написания формулы добавил возможность ее скопировать

---
+++ AreaCalc/TipWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.DB.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AreaCalc
+{
+    /// <summary>
+    /// Логика взаимодействия для TipWindow.xaml
+    /// </summary>
+    public partial class TipWindow : Window
+    {
+        public TipWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void CopyFormulaButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Копируем текст формулы в буфер обмена
+            Clipboard.SetText(FormulaTextBox.Text);
+            MessageBox.Show("Формула скопирована в буфер обмена!");
+        }
+
+
+
+    }
+}
